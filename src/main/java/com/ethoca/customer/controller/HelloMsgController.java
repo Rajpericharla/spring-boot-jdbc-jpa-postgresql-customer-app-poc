48f8a5045b70b177126099b13864f517f5edf948
rajpericharla <38297192+Rajpericharla@users.noreply.github.com>
2021-12-19 22:03:20 -0600

    Update HelloMsgController.java

--- src/main/java/com/ethoca/customer/controller/HelloMsgController.java
+++ src/main/java/com/ethoca/customer/controller/HelloMsgController.java
@@ -1,4 +1,4 @@
-package com.echoca.customer.controller;
+package com.ethoca.customer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
