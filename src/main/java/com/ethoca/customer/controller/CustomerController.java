f7ca50401b6119a65ff427a205e4ff70cc6c5813
rajpericharla <raj.pericharla@gmail.com>
2021-12-19 23:58:06 -0600

    ..

--- src/main/java/com/ethoca/customer/controller/CustomerController.java
+++ src/main/java/com/ethoca/customer/controller/CustomerController.java
@@ -17,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,22 +38,18 @@ public class CustomerController {
 	@Autowired
 	private CustomerDao customerDao;
 
-	@PostMapping("/insertCustomers")
-	public ResponseEntity<String> insertCustomers() {
+	@PostMapping("/insertCustomers/{start}/{end}")
+	public ResponseEntity<String> insertCustomers(@PathVariable("start") int start, @PathVariable("end") int end) {
 		System.out.println("insertCustomers()");
 		try {
-			Thread t = new Thread(() -> {
-				List<Customer> customerList = new ArrayList<>();
-				for (int i = 1; i <= 2000000; i++) {
-					customerList.add(new Customer(i, "Echoca#" + i, "BF-US#" + i, "US#" + i));
-					if (i % 50000 == 0) {
-						customerRepository.saveAll(customerList);
-						customerList.clear();
-					}
+			List<Customer> customerList = new ArrayList<>();
+			for (int i = start; i <= end; i++) {
+				customerList.add(new Customer(i, "Echoca#" + i, "BF-US#" + i, "US#" + i));
+				if (i % 10000 == 0) {
+					customerRepository.saveAll(customerList);
+					customerList.clear();
 				}
-			});
-			t.start();
-			
+			}
 			return new ResponseEntity<>("SUCCESS", HttpStatus.CREATED);
 		} catch (Exception e) {
 			System.out.println("Exception: " + e);
